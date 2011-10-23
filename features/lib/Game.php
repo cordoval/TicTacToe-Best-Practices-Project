a0465e06a3748438891066d433f15ce3bb2452d2
Luis Cordova <cordoval@gmail.com>
2011-10-22 19:21:05 -0500

    declared some constants and worked a bit on play method

--- features/lib/Game.php
+++ features/lib/Game.php
@@ -6,8 +6,20 @@
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\Event;
 
+/**
+ * Game class
+ *
+ * Is the master class from where the game is played.
+ * It executes the game with a main run method.
+ * The API is defined through a subset of Game methods defined in this class.
+ *
+ */
 class Game
 {
+    const PLAYER_WINS = 1;
+    const KEEP_PLAYING = 0;
+    const INVALID_POSITION = -1;
+
     protected $turnSwitcher;
     protected $turnToPlayer = 1;
     protected $players;
@@ -46,16 +58,28 @@ class Game
             // register the event
             $dispatcher->addListener('turnswitcher.action', array($listener, 'onTurnSwitcherAction'), 0);
 
-            // register the event - closure form
-            //$dispatcher->addListener('turnswitcher.action', function(Event $event) {
-            //   echo 'I handle event '.$event->getName();
-            //}, 0 ); // 0 priority
-
             // fire up the event
             $eventDispatcher->notify(
                 new Event(null, 'xyz') // no subject so sets to null
             );
 
+        }
+    }
+
+    public function play($position, $player) {
+        // 1. validatePosition
+        if (!$position->validToPlayer($player)) {
+            return self::INVALID_POSITION;
+        }
+
+        // 2. take field @ position
+        
+
+        // 3. game over criteria
+        if ('gameover') {
+            return self::PLAYER_WINS;
+        } else {
+            return self::KEEP_PLAYING;
         }
     }
 
