4a94fa2684d06df8d11f0307c453d2ffbf23c7a2
Luis Cordova <cordoval@gmail.com>
2012-01-23 18:12:07 -0500

    broken spec

--- lib/PHPPeru/TicTacToe/Spec/BoardSpec.php
+++ lib/PHPPeru/TicTacToe/Spec/BoardSpec.php
@@ -18,4 +18,12 @@ class DescribeBoard extends \PHPSpec\Context
        $this->board->markPosition(1, 'x');
        $this->board->getPosition(1)->should->be('x');
     }
+
+    function itMarksAPositionWhenNotTaken()
+    {
+       $position = 1;
+       $this->board->getPosition($position)->should->be('null');
+       $this->board->markPosition($position, 'x');
+       $this->board->getPosition(1)->should->be('x');
+    }
 }
