59d942a5406e4660d7aa538242fb38d29bbf22de
Luis Cordova <cordoval@gmail.com>
2012-02-09 03:12:47 -0500

    added sismo configuration

---
+++ sismo.config.php
@@ -0,0 +1,23 @@
+<?php
+
+$notifier = new Sismo\DBusNotifier();
+
+// add a project with custom settings
+$sf2 = new Sismo\Project('tictactoe');
+$sf2->setRepository('/home/cordoval/sites-2/tictactoeperu');
+$sf2->setBranch('develop');
+
+// phpspec command
+$phpspecCommand = '/home/cordoval/sites-2/tictactoeperu/phpspec-composer.php';
+$phpspecFolder = '/home/cordoval/sites-2/tictactoeperu/lib/PHPPeru/TicTacToe/Spec';
+//$longCommand = 'find ' . $phpspecFolder . '*.php -exec ' . $phpspecCommand . ' \'{}\' -f documentation --color \\;';
+$longCommand = $phpspecCommand . ' ' . $phpspecFolder . '-f documentation --color';
+
+// sets command, slug, commit links and notifier
+$sf2->setCommand($longCommand);
+$sf2->setSlug('tictactoe');
+$sf2->setUrlPattern('http://localhost:8000/?p=.git;a=commitdiff;h=%commit%');
+$sf2->addNotifier($notifier);
+
+return $sf2;
+
